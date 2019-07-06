81ede69b9866a6719cf0f172c06728054c38187d
crdx <crdx@users.noreply.github.com>
2019-07-06 10:46:48 +0100

    Switch to 4-space indentation
    A higher indentation level encourages less nesting, because it looks
    so jarring.

--- lib/patter/source.rb
+++ lib/patter/source.rb
@@ -1,11 +1,11 @@
 module Patter
-  class Source < Array
-    def get_samples n
-      self.sample(n).map { |str| Sample.new(str) }
-    end
+    class Source < Array
+        def get_samples n
+            self.sample(n).map { |str| Sample.new(str) }
+        end
 
-    def get_sample
-      get_samples(1).first
+        def get_sample
+            get_samples(1).first
+        end
     end
-  end
 end
